902772e4049941c27797c651bf8a988e8da0155a
Samuel  Batista Martins <s.martins@kigroup.de>
2022-06-15 10:01:58 +0100

    added middleware to protect routes and pages

---
+++ pages/_middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+const signedInPages = ["/", "/playlist", "/library"];
+
+export default function middleware(req) {
+  const { pathname, origin } = req.nextUrl;
+  if (signedInPages.find((p) => p === pathname)) {
+    const token = req.cookies.AERO_MUSIC_ACCESS_TOKEN;
+    if (!token) {
+      return NextResponse.redirect(`${origin}/signin`);
+    }
+  }
+}
