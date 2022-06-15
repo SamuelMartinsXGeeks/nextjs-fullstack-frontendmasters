10e59c45de2620271ea7db6910377ad19ddcf3dd
Samuel  Batista Martins <s.martins@kigroup.de>
2022-06-15 14:24:13 +0100

    artists ui layout

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,33 +2,6 @@ import jwt from "jsonwebtoken";
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "./prisma";
 
-// export const validateRoute = (handler) => {
-//   return async (req: NextApiRequest, res: NextApiResponse) => {
-//     const { AERO_MUSIC_ACCESS_TOKEN: token } = req.cookies;
-
-//     if (token) {
-//       let user;
-//       console.log("Token found");
-//       try {
-//         const { id } = jwt.verify(token);
-//         user = await prisma.user.findUnique({ where: { id } });
-//         if (!user) {
-//           throw new Error("User does not exist");
-//         }
-//       } catch (error) {
-//         res.status(401);
-//         res.json({ error: "Not Authorized" });
-//       }
-//       console.log("user");
-//       console.log(user);
-//       return handler(req, res, user);
-//     }
-
-//     res.status(401);
-//     res.json({ error: "Not Authorized" });
-//   };
-// };
-
 export const validateRoute = (handler) => {
   return async (req: NextApiRequest, res: NextApiResponse) => {
     const token = req.cookies.AERO_MUSIC_ACCESS_TOKEN;
